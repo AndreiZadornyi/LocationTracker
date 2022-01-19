7bb86ad526582451668e0f58ae16c4aede5261ce
andrei <andrei@andrei-pc.zws>
2022-01-19 22:58:27 +0300

    Small fix

--- app/src/main/java/com/example/myapplication3/LocationActivity.kt
+++ app/src/main/java/com/example/myapplication3/LocationActivity.kt
@@ -1,28 +1,13 @@
 package com.example.myapplication3
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.myapplication3.adapters.Item
 import com.example.myapplication3.adapters.ItemListAdapter
 import com.example.myapplication3.models.LogItem
 import com.example.myapplication3.services.TrackService
 import com.example.myapplication3.utils.Preferences
-import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_location.*
-import java.util.concurrent.TimeUnit
 
 class LocationActivity : AppCompatActivity() {
     private var adapter: ItemListAdapter? = null
